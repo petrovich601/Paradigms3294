fc973816a8948214e106146f862af2d0b27646e6
petrovich601 <114338320+petrovich601@users.noreply.github.com>
2023-09-10 16:48:42 +0500

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,22 @@
+# def multiplication(x, y):
+#     mult = x * y
+#     return mult
+#
+#
+# x = 3
+# y = 5
+# for i in range(1, x+1):
+#     for j in range(1, y+1):
+#         print(f"{i} * {j} = {multiplication(i, j)}")
+
+# Структурный подход, так как такой код короче, а процедурный подход
+# x = 9
+# y = 9
+# for i in range(1, x+1):
+#     for j in range(1, y+1):
+#         print(f"{i} * {j} = {i * j}")
+
+n = 9
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(f"{i} * {j} = {i * j}")
