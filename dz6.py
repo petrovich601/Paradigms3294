30b28e5cf7f65937e7fe06fa68372253ef0956d2
petrovich601 <114338320+petrovich601@users.noreply.github.com>
2023-09-24 13:55:04 +0500

    Add files via upload

---
+++ dz6.py
@@ -0,0 +1,38 @@
+def binary_search(arr, target):
+    def binary_search_recursive(arr, target, left, right):
+        if left > right:
+            return -1
+
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            return binary_search_recursive(arr, target, mid + 1, right)
+        else:
+            return binary_search_recursive(arr, target, left, mid - 1)
+
+    left, right = 0, len(arr) - 1
+    return binary_search_recursive(arr, target, left, right)
+
+arr = [1, 3, 5, 7, 9, 11, 13, 15]
+
+try:
+    target = int(input("Введите искомое число: "))
+except ValueError:
+    print("Ошибка: Введите целое число.")
+    exit(1)
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Искомый элемент {target} найден в массиве по индексу {result}.")
+else:
+    print(f" '-1' Искомый элемент {target} не найден в массиве.")
+
+
+
+result = []
+while (len(arr) > 0):
+    result.append(min(arr))
+    arr.remove(min(arr))
